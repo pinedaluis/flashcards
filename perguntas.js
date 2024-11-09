6701c8e5216f154ff09286289a83317b2a6f3b1d
pinedaluis <142420164+pinedaluis@users.noreply.github.com>
2024-11-09 18:47:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'Paris.'
+)
+
+criaCartao(
+    'Historia',
+    'Quem descobriu a América?',
+    'Cristóvão Colombo.'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual é a fórmula da área do triângulo?',
+    'A = (base × altura) / 2.'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é a fotossíntese?',
+    'Processo pelo qual as plantas produzem seu alimento utilizando luz solar.'
+)
+criaCartao(
+    'Física',
+    'O que é a inércia?',
+    ' A tendência de um corpo a manter seu estado de movimento ou repouso.'
+)
+criaCartao(
+    'Química',
+    'Qual é o símbolo químico do oxigênio?',
+    'O simbolo é " O "'
+)
